892bb380713051dc46978868bd8b8ff859083bed
bananawooyu <pjh9257@naver.com>
2024-11-25 06:39:04 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/kb_bank_instruqt/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "${var.prefix}_bucket"
+  bucket_prefix = "${var.prefix}-bucket"
 
 }
